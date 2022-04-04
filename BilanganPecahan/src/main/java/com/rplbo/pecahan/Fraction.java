e8d423f0d8d9e0ee55978361d90bd325894636fa
gheanatasya <78341297+gheanatasya@users.noreply.github.com>
2022-04-04 16:31:25 +0700

    Add files via upload

---
+++ BilanganPecahan/src/main/java/com/rplbo/pecahan/Fraction.java
@@ -0,0 +1,34 @@
+package com.rplbo.pecahan;
+
+public class Fraction extends Mathematics{
+    public int, int Fraction;
+
+    private int calculateDenominator(int, int, int){}
+    private void checkMultiplesofNumbers(){}
+    (Fraction, Fraction, Fraction){}
+
+    @Override
+    public com.rplbo.pecahan.Fraction multiplications() {
+        return super.multiplications();
+    }
+
+    @Override
+    public com.rplbo.pecahan.Fraction subtraction() {
+        return super.subtraction();
+    }
+
+    ubtraction(Fraction, Fraction, Fraction){}
+
+    @Override
+    public com.rplbo.pecahan.Fraction summation() {
+        return super.summation();
+    }
+
+    ummation(Fraction, Fraction, Fraction){}
+    public String toString(String){}
+
+    @Override
+    public com.rplbo.pecahan.Fraction division() {
+        return super.division();
+    }
+}
---
+++ BilanganPecahan/src/main/java/com/rplbo/pecahan/Mathematics.java
@@ -0,0 +1,41 @@
+package com.rplbo.pecahan;
+
+public class Mathematics {
+    private int numerator;
+    private int denominator;
+
+    public Mathematics (int numerator, int denominator){
+
+    }
+    public Fraction division(Fraction, Fraction){
+
+    }
+
+    private int factorize(int, int){
+
+    }
+
+    public int getDenominator(){
+        return this.denominator;
+    }
+
+    public int getNumerator(){
+        return this.numerator;
+    }
+
+    public Fraction multiplications(Fraction, Fraction){}
+
+    public void setDenominator(int denominator) {
+        this.denominator = denominator;
+    }
+
+    public void setNumerator(int numerator) {
+        this.numerator = numerator;
+    }
+
+    public Fraction simplifyFraction(int numerator, int denominator){}
+
+    public Fraction subtraction(Fraction, Fraction){}
+    public Fraction summation(Fraction, Fraction){}
+}
+
